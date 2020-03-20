c714724216d0e9c806f685529d3e83987c385c85
Jeffrowetull <jeffrowetull@gmail.com>
2020-03-20 11:14:43 -0700

    sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,16 +4,16 @@ from world import World
 
 import random
 from ast import literal_eval
-
+from util import Stack, Queue
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_line.txt"
+#map_file = "maps/test_cross.txt"
+#map_file = "maps/test_loop.txt"
+#map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -26,9 +26,26 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+#print(room_graph)
+reverse_dir = {'n':'s','s':'n','e':'w','w':'e'}
 traversal_path = []
+reverse_path =[]
+rooms ={}
+rooms[0]=player.current_room.get_exits()
 
+while len(rooms) < len(room_graph)-1:
+    if player.current_room.id not in rooms:
+        rooms[player.current_room.id] = player.current_room.get_exits()
+        last_dir = reverse_path[-1]
+        rooms[player.current_room.id].remove(last_dir)
+    while len(rooms[player.current_room.id]) < 1:
+        reverse = reverse_path.pop()
+        traversal_path.append(reverse)
+        player.travel(reverse)
+    exit_dir = rooms[player.current_room.id].pop(0)
+    traversal_path.append(exit_dir)
+    reverse_path.append(reverse_dir[exit_dir])
+    player.travel(exit_dir)
 
 
 # TRAVERSAL TEST
